27fc04008d65896e0c57e03f8c3f40047202d5c7
matheus <matheusnl2009@gmail.com>
2026-08-16 22:59:07 -0300

    Delete CashPilot2 directory

--- CashPilot2/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-
-if (!empty($_SESSION['usuario_id'])) {
-    header('Location: pages/dashboard.php');
-} else {
-    header('Location: pages/login.php');
-}
-exit;
